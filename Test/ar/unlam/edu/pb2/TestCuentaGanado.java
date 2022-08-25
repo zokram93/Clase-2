076596beb122d13fb978f1173e4c6c6b7dbed663
ZOK <mmartinez528@alumno.edu.ar>
2022-08-25 10:03:17 -0300

    Se agregar el Proyecto

---
+++ Test/ar/unlam/edu/pb2/TestCuentaGanado.java
@@ -0,0 +1,56 @@
+package ar.unlam.edu.pb2;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestCuentaGanado {
+
+	@Test
+	public void queSePuedaCrearUnCuentaGanado() {
+		CuentaGanado cuentaGanado = new CuentaGanado();
+		assertNotNull(cuentaGanado);
+	}
+
+	@Test
+
+	public void queSeCreeUnCuentaGanadoConElContadorEnCero() {
+		CuentaGanado cuentaGanado = new CuentaGanado();
+		Integer valorEsperado = 0;
+		Integer valorObtenido = cuentaGanado.getContador();
+		assertEquals(valorEsperado, valorObtenido);
+	}
+
+	@Test
+	public void queSeSumaUno() {
+		CuentaGanado cuentaGanado = new CuentaGanado();
+		Integer valorEsperado = 1;
+		cuentaGanado.sumarUno();
+		Integer valorObtenido = cuentaGanado.getContador();
+		assertEquals(valorEsperado, valorObtenido);
+	}
+
+	@Test
+	public void queSeReiniciaPasandoElLimite() {
+		CuentaGanado cuentaGanado = new CuentaGanado();
+		Integer valorEsperado = 0;
+		cuentaGanado.setLimite(9);
+		for (int i = 0; i < 10; i++) {
+			cuentaGanado.sumarUno();
+		}
+		Integer valorObtenido = cuentaGanado.getContador();
+		assertEquals(valorEsperado, valorObtenido);
+
+	}
+	
+	@Test
+	public void queSeReinicieElCuentaGanado() {
+		CuentaGanado cuentaGanado = new CuentaGanado();
+		Integer valorEsperado = 0;
+		cuentaGanado.sumarUno();
+		cuentaGanado.reiniciarLimite();
+		Integer valorObtenido = cuentaGanado.getContador();
+		assertEquals(valorEsperado, valorObtenido);
+	}
+
+}
---
+++ src/ar/unlam/edu/pb2/CuentaGanado.java
@@ -0,0 +1,31 @@
+package ar.unlam.edu.pb2;
+
+public class CuentaGanado {
+	private Integer contador;
+	private Integer limite;
+
+	public CuentaGanado() {
+		this.contador = 0;
+	}
+
+	public Integer getContador() {
+		return this.contador;
+	}
+	public void setLimite(Integer limite){
+		this.limite = limite;
+	}
+
+	public void sumarUno() {
+		if(this.contador < this.limite){
+		this.contador++;
+		} else {
+			reiniciarLimite();
+		}
+	}
+	
+	public void reiniciarLimite(){
+			contador = 0;
+	}
+	
+
+}
