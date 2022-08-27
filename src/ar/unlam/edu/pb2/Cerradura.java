81340c38e420fb777900c22dbef0038cadc90c14
ZOK <mmartinez528@alumno.edu.ar>
2022-08-26 23:25:16 -0300

    Cerradura v.1

--- src/ar/unlam/edu/pb2/Cerradura.java
+++ src/ar/unlam/edu/pb2/Cerradura.java
@@ -1,5 +1,29 @@
 package ar.unlam.edu.pb2;
 
 public class Cerradura {
+	private Integer clave;
+	private Integer cantidadIntentos;
+	private Boolean estado;
 
+	public Cerradura(Integer clave, Integer cantidadIntentos) {
+		this.clave = clave;
+		this.cantidadIntentos = cantidadIntentos;
+		this.estado = false;
+	}
+
+	public boolean consultaEstadoCerradura() {
+		if (this.estado != false)
+			return true;
+		return false;
+	}
+	
+	public boolean abrirCerradura (Integer clave) {
+		if(this.clave.equals(clave)) {
+			this.cantidadIntentos++;
+			return true;
+		}
+
+		return false;
+	}
+	
 }
